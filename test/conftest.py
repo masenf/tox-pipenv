3c90644c6c10e05c39043b689ee48ebafd5315f2
Masen Furer <m_github@0x26.net>
2022-11-10 01:21:38 -0800

    conftest: parametrize venv fixture
    fixtures `has_pipfile` and `has_pipfile_lock` are
    parametrized and modify `venv` to create a test
    matrix of option combinations.
    
    move all mock object properties to instance variables
    to avoid leaking data across test cases

--- test/conftest.py
+++ test/conftest.py
@@ -1,31 +1,36 @@
 import pytest
-import py
 import subprocess
-import os
+
+
+class MockOption(object):
+    def __init__(self):
+        self.pipenv_lock = False
 
 
 class MockConfig(object):
     def __init__(self, tmpdir):
         self._tmpdir = tmpdir
-
-    """
-    Fake tox config with static values
-    """
-    toxinidir = os.curdir
+        self.option = MockOption()
+        self.toxinidir = tmpdir / "tox-ini-dir"
+        self.toxinidir.ensure(dir=True)
 
 
 class MockEnvironmentConfig(object):
-    sitepackages = False
-    envdir = None
-    pip_pre = False
-    envname = "mock"
+    def __init__(self, tmpdir, config):
+        self.config = config
+        self.envname = "mock"
+        self.envdir = tmpdir / "dot-tox" / self.envname
+        self.envdir.ensure(dir=True)
+        self.sitepackages = False
+        self.pip_pre = False
+        self.skip_pipenv = False
+        self.pipenv_install_opts = None
+        self.pipenv_install_cmd = None
 
 
 class MockSession(object):
     def __init__(self, tmpdir):
         self.config = MockConfig(tmpdir)
-        self.config.toxinidir = tmpdir / "tox-ini-dir"
-        self.config.toxinidir.ensure(dir=True)
 
     def make_emptydir(self, path):
         return True
@@ -35,10 +40,10 @@ class MockVenv(object):
     def __init__(self, tmpdir, *args, **kwargs):
         self.tmpdir = tmpdir
         self.session = MockSession(tmpdir)
-        self.envconfig = MockEnvironmentConfig()
-        self.envconfig.envdir = tmpdir / "dot-tox" / self.envconfig.envname
-        self.envconfig.envdir.ensure(dir=True)
+        self.envconfig = MockEnvironmentConfig(tmpdir, config=self.session.config)
         self.deps = []
+        self._pipfile = None
+        self._pipfile_lock = None
 
     @property
     def path(self):
@@ -73,10 +78,37 @@ class MockAction(object):
 
 @pytest.fixture
 def venv(tmpdir):
-    venv = MockVenv(tmpdir)
-    return  venv
+    return MockVenv(tmpdir)
+
+
+@pytest.fixture(params=[True, False], ids=["has_Pipfile", "no_Pipfile"])
+def has_pipfile(request, venv):
+    if request.param:
+        venv._pipfile = venv.session.config.toxinidir / "Pipfile"
+        venv._pipfile.ensure()
+    return request.param
+
+
+@pytest.fixture(params=[True, False], ids=["has_Pipfile.lock", "no_Pipfile.lock"])
+def has_pipfile_lock(request, venv):
+    if request.param:
+        venv._pipfile_lock = venv.session.config.toxinidir / "Pipfile.lock.{}".format(venv.envconfig.envname)
+        venv._pipfile_lock.ensure()
+    return request.param
+
+
+@pytest.fixture(params=[True, False], ids=["has_pip_pre", "no_pip_pre"])
+def has_pip_pre(request, venv):
+    venv.envconfig.pip_pre = request.param
+    return request.param
 
 
 @pytest.fixture
 def actioncls():
     return MockAction
+
+
+@pytest.fixture
+def action(venv, actioncls):
+    return actioncls(venv)
+
