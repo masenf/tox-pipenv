793820a91ea86daa8b58dd1b0f559e24555b57ed
Masen Furer <m_github@0x26.net>
2022-11-11 14:45:55 -0800

    unit/test_addoption: smoke test for coverage
    ensure that `unit` tests provide 100% coverage of
    the plugin code

---
+++ test/unit/test_addoption.py
@@ -0,0 +1,10 @@
+import tox_pipenv.plugin
+
+
+def test_tox_addoption_smoke(mocker):
+    parser = mocker.Mock()
+    tox_pipenv.plugin.tox_addoption(parser)
+    assert parser.add_argument.mock_calls[0].args == ("--pipenv-update",)
+    assert parser.add_testenv_attribute.mock_calls[0].args == ("skip_pipenv",)
+    assert parser.add_testenv_attribute.mock_calls[1].args == ("pipenv_install_cmd",)
+    assert parser.add_testenv_attribute.mock_calls[2].args == ("pipenv_install_opts",)
