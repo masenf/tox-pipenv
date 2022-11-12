b727628eb324356a7d791296a80d11cb50b7c8e4
Masen Furer <m_github@0x26.net>
2022-11-11 20:58:39 -0800

    test_addoptions: py3.7 compatible mock assertions

--- test/unit/test_addoption.py
+++ test/unit/test_addoption.py
@@ -4,6 +4,6 @@ import tox_pipenv.plugin
 def test_tox_addoption_smoke(mocker):
     parser = mocker.Mock()
     tox_pipenv.plugin.tox_addoption(parser)
-    assert parser.add_argument.mock_calls[0].args == ("--pipenv-update",)
-    assert parser.add_testenv_attribute.mock_calls[0].args == ("skip_pipenv",)
-    assert parser.add_testenv_attribute.mock_calls[1].args == ("pipenv_install_opts",)
+    assert parser.add_argument.call_args[0] == ("--pipenv-update",)
+    assert parser.add_testenv_attribute.call_args_list[0][0] == ("skip_pipenv",)
+    assert parser.add_testenv_attribute.call_args_list[1][0] == ("pipenv_install_opts",)
